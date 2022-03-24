4126a5ddfc45a7b56e43e5206e572895ac0deb79
Joli Holmes <holmesjoli@gmail.com>
2022-03-24 00:54:37 -0400

    modified timer to change every 10th of a second

--- js/helper_functions.js
+++ js/helper_functions.js
@@ -1,20 +1,10 @@
 function buildTimer() {
     var totalSeconds = 0;
     
-    function pad(val) {
-        var valString = val + "";
-        if (valString.length < 2) {
-            return "0" + valString;
-        } else {
-            return valString;
-        }
-    };
-    
     function setTime() {
         ++totalSeconds;
-        document.getElementById('seconds').innerHTML = pad(totalSeconds % 60);
-        document.getElementById('minutes').innerHTML = pad(parseInt(totalSeconds / 60));
+        document.getElementById('days').innerHTML = `${totalSeconds} days`;
     };
     
-    setInterval(setTime, 1000);
+    setInterval(setTime, 100);
 };
