f49b2c7613a04f95fc474cca31e14e16b39196a1
Joli Holmes <holmesjoli@gmail.com>
2022-03-29 11:45:30 -0400

    set-up promises to load multiple data files

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-d3.csv("./data/data.csv").then(function(data) {
+const files = [
+    { "type": "csv", "file": "./data/data.csv" },
+    { "type": "csv", "file": "./data/dates.csv" } // dataset of every earthquake on Mar 21, 2022 from here: https://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
+];
+let promises = [];
 
-    console.log(data);
-
-
-
+files.forEach(function (d) {
+    if (d.type == "json") {
+        promises.push(d3.json(d.file));
+    } else {
+        promises.push(d3.csv(d.file));
+    }
 });
+
+Promise.all(promises).then(function (values) {
+    drawVis(values[0], values[1])
+});
+
+function drawVis(data, dates) {
+
+    
+
+        console.log(data);
+        console.log(dates);
+
+}
 
 buildTimer();
