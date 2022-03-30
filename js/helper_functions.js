57f5c09f5a01b38e862d9a0fe773e3d64b05c3fd
Joli Holmes <holmesjoli@gmail.com>
2022-03-29 20:39:20 -0400

    simplified function

--- js/helper_functions.js
+++ js/helper_functions.js
@@ -21,7 +21,7 @@ function timer(dates) {
     let datesExtent = d3.extent(dates, function(d) {return +d.i});
     let start = datesExtent[0];
     let end = datesExtent[1];
-    var counter = start;
+    let counter = start;
     let monthId = document.getElementById('month');
     let dayId = document.getElementById('days');
     let playPause = document.getElementById("play-pause");
@@ -30,28 +30,23 @@ function timer(dates) {
 
     let play = true;
 
-    function buildTimer() {
+    function setTime() {
+        if (counter < end) {
 
-        function setTime() {
-            if (counter < end) {
+            let filteredDates = dates.filter(function(d) {
+                return d.i === counter;
+            });
+            dayId.innerHTML = `${filteredDates[0].day}`;
+            monthId.innerHTML = `${filteredDates[0].month_name}`;
 
-                let filteredDates = dates.filter(function(d) {
-                    return d.i === counter;
-                });
-                dayId.innerHTML = `${filteredDates[0].day}`;
-                monthId.innerHTML = `${filteredDates[0].month_name}`;
-
-                if (play) {
-                    counter++;
-                }
-            } else {
-                clearInterval(monthId);
-                clearInterval(dayId);
+            if (play) {
+                counter++;
             }
+        } else {
+            clearInterval(monthId);
+            clearInterval(dayId);
         }
-
-        setInterval(setTime, 250);
-    };
+    }
 
     reset.addEventListener("click", function(e) {
         counter = start;
@@ -67,5 +62,5 @@ function timer(dates) {
         }
     });
 
-    buildTimer();
+    setInterval(setTime, 250);
 }
