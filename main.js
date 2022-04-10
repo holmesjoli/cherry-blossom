31902df27e11e8dd15e886b5230b4804562af650
Joli Holmes <holmesjoli@gmail.com>
2022-04-10 19:59:59 -0400

    added flowers

--- main.js
+++ main.js
@@ -45,9 +45,38 @@ for (var key of Object.keys(files)) {
     Helper.read(fl.pth, fl.parse, promises);
 }
 
-Promise.all(promises).then(function (values) {
-    draw(values[0], values[1], values[2]);
-});
+
+let svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 500)
+
+svg.append("g")
+
+var points = [
+	[0, 80],
+	[100, 100],
+	[200, 30],
+	[300, 50],
+	[400, 40],
+	[500, 80]
+];
+
+let d = "M12.41,15.53c-.47,17.23,18.76,3.36,2.6-2.12,16.41,6.3,8.57-17.41-1.03-2.82-.09-.09-.15-.15-.27-.21C25.32-2.53-.01-3.71,10.83,10.36,.8-3.52-6.29,19.62,9.59,13.35c0,.09,.03,.18,.06,.24-16.6,4.37,3.05,19.08,2.54,1.94h.21Z"
+
+svg.select('g')
+	.selectAll('path')
+	.data(points)
+	.join('path')
+	.attr('transform', function(d) {
+		return 'translate(' + d + ')';
+	})
+    .attr('d', d);
+
+
+// Promise.all(promises).then(function (values) {
+//     draw(values[0], values[1], values[2]);
+// });
 
 function draw(data, dates, flower) {
 
